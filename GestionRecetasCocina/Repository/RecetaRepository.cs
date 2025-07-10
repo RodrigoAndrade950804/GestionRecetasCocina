eeb3079a56a6403153fc44bca3d8566d5c05f400
Rodrigo Andrade <rodrigo.andrade@hotmail.com>
2025-07-10 07:33:02 -0500

    Agregue el CRUD en el repository, mismo que va a utilizar la logica para crear la tabla de recetas y guardarla en la base de datos

--- GestionRecetasCocina/Repository/RecetaRepository.cs
+++ GestionRecetasCocina/Repository/RecetaRepository.cs
@@ -1,12 +1,69 @@
-﻿using System;
+﻿using GestionRecetasCocina.Models;
+using SQLite;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.IO;
 using System.Threading.Tasks;
 
-namespace GestionRecetasCocina.Repository
+namespace GestionRecetasCocina.Repositories
 {
-    internal class RecetaRepository
+    public class RecetaRepository
     {
+        private readonly SQLiteAsyncConnection _database;
+
+        // Constructor principal
+        public RecetaRepository(string dbPath)
+        {
+            _database = new SQLiteAsyncConnection(dbPath);
+            _database.CreateTableAsync<Receta>().Wait();
+        }
+
+        // Constructor por defecto
+        public RecetaRepository() : this(Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "recetas.db"))
+        {
+        }
+
+        // Obtener todas las recetas
+        public Task<List<Receta>> GetRecetasAsync()
+        {
+            return _database.Table<Receta>().ToListAsync();
+        }
+
+        // Obtener receta por ID
+        public Task<Receta> GetRecetaByIdAsync(int id)
+        {
+            return _database.Table<Receta>()
+                            .Where(r => r.Id == id)
+                            .FirstOrDefaultAsync();
+        }
+
+        // Agregar nueva receta
+        public Task<int> AddRecetaAsync(Receta receta)
+        {
+            return _database.InsertAsync(receta);
+        }
+
+        // Actualizar receta existente
+        public Task<int> UpdateRecetaAsync(Receta receta)
+        {
+            return _database.UpdateAsync(receta);
+        }
+
+        // Eliminar receta por objeto
+        public Task<int> DeleteRecetaAsync(Receta receta)
+        {
+            return _database.DeleteAsync(receta);
+        }
+
+        // Eliminar receta por ID
+        public async Task<int> DeleteRecetaByIdAsync(int id)
+        {
+            var receta = await GetRecetaByIdAsync(id);
+            if (receta != null)
+                return await DeleteRecetaAsync(receta);
+            return 0;
+        }
     }
 }
+
